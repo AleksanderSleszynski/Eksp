441171cb6aab69fa0a379a255f641b60bb1fbaa4
Rasmuhamed <a.sleszynski@gmail.com>
2015-03-20 00:30:07 +0100

    Menu setup

--- app/src/main/java/com/example/julian/eksperymentowo/MainActivity.java
+++ app/src/main/java/com/example/julian/eksperymentowo/MainActivity.java
@@ -1,40 +1,53 @@
 package com.example.julian.eksperymentowo;
 
-import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.GridView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
 
-//    RecyclerView mRecyclerView;
-//    RecyclerView.LayoutManager mLayoutManager;
-//    RecyclerView.Adapter mAdapter;
-
-    Context context;
-    public static int[] mainListName={R.string.dry_ice, R.string.balloons, R.string.polymer};
-    public static int [] mainListImg={R.drawable.dry_ice_1_img, R.drawable.balloons_img, R.drawable.polymer_img};
+//    private GridView gridView;
+//    private GridViewAdapter mAdapter;
+//
+//    public static int[] mainListName={R.string.dry_ice, R.string.balloons, R.string.polymer, R.string.polymer, R.string.polymer, R.string.polymer, R.string.polymer,
+//            R.string.polymer, R.string.polymer, R.string.polymer, R.string.polymer};
+//    public static int [] mainListImg={R.drawable.dry_ice_1_img, R.drawable.balloons_img, R.drawable.polymer_img, R.drawable.polymer_img, R.drawable.polymer_img,
+//            R.drawable.polymer_img, R.drawable.polymer_img, R.drawable.polymer_img, R.drawable.polymer_img, R.drawable.polymer_img, R.drawable.polymer_img};
+//    private Integer[] mHeaderPosition = {1,5};
+//    private String[] mHeaderNames = {"suchy lód", "Polimer"};
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyAdapter adapter = new MyAdapter(this, generateData());
 
-        GridView gridView = (GridView) findViewById(R.id.grid_view);
-        gridView.setAdapter(new GridViewAdapter(this, mainListName, mainListImg));
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+}
 
+    private ArrayList<Model> generateData() {
+        ArrayList<Model> models = new ArrayList<Model>();
+        models.add(new Model("Suchy lod"));
+        models.add(new Model(R.drawable.dry_ice_1_img,"Banki"));
+        models.add(new Model(R.drawable.dry_ice_1_img,"Kalafior"));
+        models.add(new Model(R.drawable.dry_ice_1_img,"Pilka"));
+        models.add(new Model(R.drawable.dry_ice_1_img,"Kolby"));
 
-//        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-//        mRecyclerView.setHasFixedSize(true);
-//
-//        mLayoutManager = new LinearLayoutManager(this);
-//        mRecyclerView.setLayoutManager(mLayoutManager);
-//
-//        mAdapter = new CardAdapter();
-//        mRecyclerView.setAdapter(mAdapter);
+        models.add(new Model("Balony"));
+        models.add(new Model(R.drawable.balloons_img,"Szaszlyk"));
+        models.add(new Model(R.drawable.balloons_img,"Ogien"));
 
+        models.add(new Model("Polimer"));
+        models.add(new Model(R.drawable.polymer_img,"Sztuczny snieg"));
+        models.add(new Model(R.drawable.polymer_img,"Pampers"));
+
+        return models;
     }
 
     @Override
@@ -58,4 +71,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
