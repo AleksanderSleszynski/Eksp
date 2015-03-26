375dfc9a0f8e5c4a2f9eb2098f5d5af3023ce4d2
Rasmuhamed <a.sleszynski@gmail.com>
2015-03-26 14:01:17 +0100

    Add new activity, intents

---
+++ app/src/main/java/com/example/julian/eksperymentowo/ExperimentList.java
@@ -0,0 +1,61 @@
+package com.example.julian.eksperymentowo;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class ExperimentList extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_experiment_list);
+
+        Button b1 = (Button) findViewById(R.id.button_experiment_list_done);
+        Button b2 = (Button) findViewById(R.id.button_experiment_list_lead);
+
+        final Intent i = new Intent(this, Experiment.class);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_dry_ice_1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/julian/eksperymentowo/MainActivity.java
+++ app/src/main/java/com/example/julian/eksperymentowo/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.julian.eksperymentowo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.GridView;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,15 +14,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
-//    private GridView gridView;
-//    private GridViewAdapter mAdapter;
-//
-//    public static int[] mainListName={R.string.dry_ice, R.string.balloons, R.string.polymer, R.string.polymer, R.string.polymer, R.string.polymer, R.string.polymer,
-//            R.string.polymer, R.string.polymer, R.string.polymer, R.string.polymer};
-//    public static int [] mainListImg={R.drawable.dry_ice_1_img, R.drawable.balloons_img, R.drawable.polymer_img, R.drawable.polymer_img, R.drawable.polymer_img,
-//            R.drawable.polymer_img, R.drawable.polymer_img, R.drawable.polymer_img, R.drawable.polymer_img, R.drawable.polymer_img, R.drawable.polymer_img};
-//    private Integer[] mHeaderPosition = {1,5};
-//    private String[] mHeaderNames = {"suchy lód", "Polimer"};
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,15 +23,25 @@ public class MainActivity extends ActionBarActivity {
 
         ListView listView = (ListView) findViewById(R.id.listview);
         listView.setAdapter(adapter);
+
+        final Intent i = new Intent(this, ExperimentList.class);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // Start your Activity according to the item just clicked.
+                startActivity(i);
+            }
+        });
+
 }
 
     private ArrayList<Model> generateData() {
         ArrayList<Model> models = new ArrayList<Model>();
         models.add(new Model("Suchy lod"));
-        models.add(new Model(R.drawable.dry_ice_1_img,"Banki"));
-        models.add(new Model(R.drawable.dry_ice_1_img,"Kalafior"));
-        models.add(new Model(R.drawable.dry_ice_1_img,"Pilka"));
-        models.add(new Model(R.drawable.dry_ice_1_img,"Kolby"));
+        models.add(new Model(R.drawable.dry_ice_kolby_i_zlewki_img,"Kolby i Zlewki"));
+        models.add(new Model(R.drawable.dry_ice_czarodziejska_kula_img,"Czarodziejka kula"));
+        models.add(new Model(R.drawable.dry_ice_generator_piany_img,"Generator Piany"));
+
 
         models.add(new Model("Balony"));
         models.add(new Model(R.drawable.balloons_img,"Szaszlyk"));
@@ -49,6 +53,7 @@ public class MainActivity extends ActionBarActivity {
 
         return models;
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
